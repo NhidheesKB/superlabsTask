01b24143ecd042e2664dd73c4326dd3d5edefe76
NhidheesKB <nhidhees@gmail.com>
2026-04-19 16:27:10 +0530

    completed the docs url error

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,13 +1,25 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 import tailwindcss from "@tailwindcss/vite";
-import { resolve } from "path"
 export default defineNuxtConfig({
   devtools: { enabled: true, vscode: { enabled: false } },
   css: ["./app/assets/css/main.css"],
   modules: ["@scalar/nuxt", "nuxt-csurf"],
   nitro: {
     experimental: { openAPI: true },
-    openAPI: { ui: { scalar: { route: "/docs"} } },
+    openAPI: {
+      ui: {
+        scalar: {
+          route: "/docs",
+          servers: [
+            {
+              url: "https://superlabs-task-nhidhees.vercel.app/",
+              description: "production",
+            },
+            { url: "http://localhost:3000/", description: "development" },
+          ],
+        },
+      },
+    },
   },
 
   vite: {
